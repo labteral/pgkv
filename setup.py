33004c11bd6b6db687820254635d51c53179ae50
brunneis <dev@brunneis.com>
2022-02-08 22:17:04 +0000

    First commit

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 
 from setuptools import setup
 from setuptools import find_packages
-import package_name
+import pgkv
 
 setup(
-    name='package_name',
-    version=package_name.__version__,
-    description='description_value',
-    url='url_value',
-    author='author_value',
-    author_email='email_value',
+    name='pgkv',
+    version=pgkv.__version__,
+    description='Use PostgreSQL as a key-value store',
+    url='https://github.com/labteral/pgkv',
+    author='Rodrigo Martínez Castaño',
+    author_email='rodrigo@martinez.gal',
     license='GNU General Public License v3 (GPLv3)',
     packages=find_packages(),
     zip_safe=False,
@@ -26,4 +26,7 @@ setup(
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
     python_requires=">=3.6",
-    install_requires=[''])  # Dependencies
+    install_requires=[
+        'psycopg2'
+    ]
+)
